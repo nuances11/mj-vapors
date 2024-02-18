586076c3944a118ede2648c55d10ed01e540c8c3
Sir Noel Hernandez <snghernandez11@gmail.com>
2024-02-18 23:22:17 +0800

    Fix Model Relation

--- backend/app/Http/Controllers/API/AttributeController.php
+++ backend/app/Http/Controllers/API/AttributeController.php
@@ -4,17 +4,50 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Attribute;
+use App\Models\AttributeOption;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 
-class AttributeController extends Controller
+class AttributeController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse|\Illuminate\Http\Resources\Json\AnonymousResourceCollection
     {
-        //
+        try {
+
+            [
+                'sort' => $sort,
+                'sort_field' => $sortField,
+                'page_limit' => $pageLimit,
+                'search_keyword' => $searchKeyword,
+                'show_all_records' => $showAllRecords,
+                'filters' => $filters
+            ] = paginatedRequest();
+
+            $query = Attribute::with('attributeOptions:id,value,attribute_id')
+//                ->query()
+                ->filter($filters)
+                ->search($searchKeyword);
+
+            if ($showAllRecords) {
+                $attributes = $query->get();
+            } else {
+                $attributes = $query->paginate($pageLimit);
+            }
+
+            return JsonResource::collection($attributes);
+
+
+        } catch(Exception $e) {
+            return response()->json([
+                "message" => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
@@ -22,7 +55,39 @@ class AttributeController extends Controller
      */
     public function store(Request $request): JsonResponse
     {
-        return response()->json($request->all());
+//        return response()->json($request->all());
+        DB::beginTransaction();
+        try {
+            $data = $request->all();
+            $name = $data['name'];
+            $slug = generateSlug($name);
+            $attribute = new Attribute();
+            $attribute->name = $name;
+            $attribute->slug = $slug;
+            $attribute->save();
+            $attribute->refresh();
+
+            if (count($data['options']) > 0) {
+//                $attribute->attributeOptions()->saveMany($data['options']);
+                foreach ($data['options'] as $option) {
+                    $attributeOption = new AttributeOption();
+                    $attributeOption->value =$option['value'];
+                    $attribute->attributeOptions()->save($attributeOption);
+                }
+            } else {
+                DB::rollBack();
+                return $this->sendError('No attribute options.', ['error' => 'No attribute options.'], 403);
+            }
+
+            DB::commit();
+
+            return $this->sendResponse($attribute, 'Attribute created');
+
+        } catch(Exception $e) {
+            DB::rollBack();
+
+            return $this->sendError($e->getMessage(), ['error' => $e->getMessage()], 500);
+        }
     }
 
     /**
--- backend/app/Models/Attribute.php
+++ backend/app/Models/Attribute.php
@@ -19,4 +19,20 @@ class Attribute extends Model
     {
         return $this->hasMany(AttributeOption::class);
     }
+
+    public function scopeSearch($query, $keyword)
+    {
+        $query->when($keyword !== '', function ($query) use ($keyword) {
+            $query->where(function ($query) use ($keyword) {
+                $query->whereRaw("name LIKE '%$keyword%'");
+            });
+        });
+    }
+
+    public function scopeFilter($query, $filters)
+    {
+        foreach ($filters as $filter_name => $filter_value) {
+            $query->where($filter_name, strtolower($filter_value));
+        }
+    }
 }
