eb4d0812614204592a3778c216f743c44d64f640
Sir Noel Hernandez <sirnoel.webdev@gmail.com>
2024-02-21 02:01:25 +0800

    Notify default config

---
+++ frontend/src/boot/notify-defaults.js
@@ -0,0 +1,15 @@
+import { boot } from 'quasar/wrappers'
+import {Notify} from "quasar";
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async (/* { app, router, ... } */) => {
+  Notify.setDefaults({
+    position: 'top',
+    timeout: 2500,
+    progress: true,
+    actions: [
+      { label: 'Dismiss', color: 'white', handler: () => { /* ... */ } }
+    ]
+  })
+})
